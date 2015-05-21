f6957e7c5c2a2b8f39458488b47b47d497a88743
ishitaprakash <smileishita25@gmail.com>
2015-05-21 17:35:35 +0530

    first commit

--- board/tpb1x.msd/version.h
+++ board/tpb1x.msd/version.h
@@ -1,4 +1,4 @@
-#define	FW_BUILD_NUMBER		60
+#define	FW_BUILD_NUMBER		62
 #define	FW_X_OR_V			'X'
 #define	FW_VERSION_NUMBER	2
 #define	FW_REVISION_NUMBER	1
@@ -7,5 +7,5 @@
 #define	FW_MESSAGE_STRING	"NOT FOR RELEASE"
 #define	BOARD_TYPE_STRING	"BIT Firmware for TP B1X/msd."
 #define	BOARD_CUTE_STRING	"CUTE Firmware for TP B1X/msd."
-#define	TIME_DATE_STRING	"Built on: Wed May 20 2015, 17:12:23"
-#define	FW_BUILD_STRING		"Build: 60"
+#define	TIME_DATE_STRING	"Built on: Thu May 21 2015, 16:50:49"
+#define	FW_BUILD_STRING		"Build: 62"
